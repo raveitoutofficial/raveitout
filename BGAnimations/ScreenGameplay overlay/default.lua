5ef6cb4ab39bd597456ce94d711492eeeea2a732
AKSYS <kirbyrulez@att.net>
2018-10-06 01:06:29 -0500

    Fail animation

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -291,24 +291,6 @@ local t = Def.ActorFrame{
 	LoadActor("MenuOptions");	
 };
 
-for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
-	t[#t+1] = LoadActor("Failed")..{
-		InitCommand=function(self)
-			local style = ToEnumShortString(GAMESTATE:GetCurrentStyle():GetStyleType())
-			if style == "OnePlayerOneSide" and PREFSMAN:GetPreference("Center1Player") == true then
-				self:x(SCREEN_CENTER_X);
-			else
-				self:x(THEME:GetMetric("ScreenGameplay","Player"..pname(pn).."OnePlayerOneSideX"));
-			end;
-			self:y(SCREEN_CENTER_Y):visible(false);
-		end;
-		ComboChangedMessageCommand=function(self,params)
-			if STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):GetFailed() then
-				self:visible(true);
-			end;
-		end;
-	
-	};
-end;
+t[#t+1] = LoadActor("playerFailed");
 
 return t;
--- BGAnimations/ScreenGameplay overlay/playerFailed.lua
+++ BGAnimations/ScreenGameplay overlay/playerFailed.lua
@@ -1,7 +1,9 @@
 local t = Def.ActorFrame{};
 
 for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
-	t[#t+1] = LoadActor("Failed")..{
+	local alreadyFailed = false;
+	t[#t+1] = Def.ActorFrame{
+	
 		InitCommand=function(self)
 			local style = ToEnumShortString(GAMESTATE:GetCurrentStyle():GetStyleType())
 			if style == "OnePlayerOneSide" and PREFSMAN:GetPreference("Center1Player") == true then
@@ -9,13 +11,38 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
 			else
 				self:x(THEME:GetMetric("ScreenGameplay","Player"..pname(pn).."OnePlayerOneSideX"));
 			end;
-			self:y(SCREEN_CENTER_Y):visible(false);
+			self:y(SCREEN_CENTER_Y+50);
 		end;
 		ComboChangedMessageCommand=function(self,params)
 			local failed = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn):GetFailed();
-			visible(failed);
+			if failed and not alreadyFailed then
+				alreadyFailed = true;
+				self:GetChild("FailedG"):playcommand("Play");
+				self:GetChild("TryAgainG"):playcommand("Play");
+				
+				--Make the entire note field invisible when tweened
+				--PlayerP1 and PlayerP2 cannot be tweened
+				SCREENMAN:GetTopScreen():GetChild('Player'..pname(pn)):visible(false);
+				
+				--Just make the notes invisible. No point in doing this, really
+				--[[local ps = GAMESTATE:GetPlayerState(pn);
+				local playerOptions = ps:GetPlayerOptions('ModsLevel_Song');
+				--:StealthPastReceptors(true); is probably a 5.1 thing but I'm still using 5.0.12 on Windows :V
+				playerOptions:Stealth(1,1, true);]]
+			end;
 		end;
 	
+		LoadActor("Failed")..{
+			Name="FailedG";
+			InitCommand=cmd(vertalign,bottom;diffusealpha,0;addx,100;);
+			PlayCommand=cmd(decelerate,.5;diffusealpha,1;addx,-100);
+		};
+		
+		LoadActor("try_again")..{
+			Name="TryAgainG";
+			InitCommand=cmd(vertalign,top;diffusealpha,0;addx,-100);
+			PlayCommand=cmd(decelerate,.5;diffusealpha,1;addx,100);
+		};
 	};
 end;
 
