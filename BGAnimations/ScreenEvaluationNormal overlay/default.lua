0fcd7eaca0a33c8657c498e8b127030b32932e00
RhythmLunatic <kirbyrulez@att.net>
2018-12-19 15:01:39 -0600

    hurr

--- BGAnimations/ScreenEvaluationNormal overlay/default.lua
+++ BGAnimations/ScreenEvaluationNormal overlay/default.lua
@@ -131,7 +131,7 @@ t[#t+1] = Def.ActorFrame{
 
 for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
 	NumHeartsLeft[pn] = NumHeartsLeft[pn]-GetNumHeartsForSong()
-	if GAMESTATE:GetNumStagesLeft(pn) =< 0 and NumHeartsLeft[pn] > 0 then
+	if GAMESTATE:GetNumStagesLeft(pn) <= 0 and NumHeartsLeft[pn] > 0 then
 		GAMESTATE:AddStageToPlayer(pn) --Hack to make sure SM5 doesn't think there are no stages left
 	end;
 end;
