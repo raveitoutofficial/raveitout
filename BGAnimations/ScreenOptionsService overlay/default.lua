e560489a93134acea57a2281d87f43e4a6f4cddd
AKSYS <kirbyrulez@att.net>
2019-01-20 12:50:10 -0600

    fix group select to service menu softlock

--- BGAnimations/ScreenOptionsService overlay/default.lua
+++ BGAnimations/ScreenOptionsService overlay/default.lua
@@ -7,6 +7,10 @@ t[#t+1] = Def.ActorFrame{
 			Combo = THEME:GetMetric("CustomRIO","MissToBreak");
 			PrevCombo = getenv("BreakCombo");
 			setenv("BreakCombo",Combo);
+			
+			--Fix for people stuck in the group select since set_input_redirected doesn't turn off
+			SCREENMAN:set_input_redirected(PLAYER_1, false)
+			SCREENMAN:set_input_redirected(PLAYER_2, false)
 		end;
 	};
 	
