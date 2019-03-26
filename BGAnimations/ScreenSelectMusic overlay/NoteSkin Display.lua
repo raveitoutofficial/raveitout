76ccb0b6d74e0c88f9497cf6ed8236ae9b9821bf
AKSYS <kirbyrulez@att.net>
2019-03-25 22:14:32 -0500

    OptionsList improvements

---
+++ BGAnimations/ScreenSelectMusic overlay/NoteSkin Display.lua
@@ -0,0 +1,46 @@
+local pn = ...
+local function CurrentNoteSkin(p)
+        local state = GAMESTATE:GetPlayerState(p)
+        local mods = state:GetPlayerOptionsArray( 'ModsLevel_Preferred' )
+        local skins = NOTESKIN:GetNoteSkinNames()
+
+        for i = 1, #mods do
+            for j = 1, #skins do
+                if string.lower( mods[i] ) == string.lower( skins[j] ) then
+                   return skins[j];
+                end
+            end
+        end
+
+end
+return Def.ActorFrame{
+		OnCommand=cmd(visible,GAMESTATE:IsHumanPlayer(pn));
+		PlayerJoinedMessageCommand=cmd(queuecommand,"On");
+		PlayerUnjoinedMessageCommand=cmd(playcommand,"On");
+		
+		LoadActor("optionIcon")..{
+			InitCommand=cmd(draworder,100;zoomy,0.34;zoomx,0.425;diffusealpha,.75);
+		};
+		
+		Def.Sprite{
+			InitCommand=cmd(x,1;draworder,100);
+			OnCommand=function(self)
+				local arrow = "UpLeft";
+				local name = "Tap note";
+				if CurrentNoteSkin(pn) == "delta" then
+					name = "Ready Receptor";
+				elseif CurrentNoteSkin(pn) == "delta-note" or CurrentNoteSkin(pn) == "rhythm" then
+					arrow = "_UpLeft";
+				end
+				local path = NOTESKIN:GetPathForNoteSkin(arrow, name, CurrentNoteSkin(pn));
+				
+				self:Load(path);
+				self:croptop(0);
+				self:cropright(0);
+				self:zoom(0.35);
+			end;
+			OptionsListClosedMessageCommand=cmd(queuecommand,"On");
+			CodeMessageCommand=cmd(playcommand,"On");
+		};
+
+	};
--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 The Official Rave It Out theme. Pitbull would approve. Dale!
 
 ## Usage
-Works on 5.1-beta2 and 5.0.12 only.
+Works on 5.1-beta2 and 5.0.12 only. Some features will be missing if you do not compile the latest 5.1-new branch of StepMania.
 
 Configure the groups for Easy and Special mode in 01 Utilities.lua.
 
 Supports Dance and Pump mode, although the graphics will still be for pump. (May support other modes too, but you'll probably get some bugs)
+
+## Features that require the latest StepMania
 
 ## Configuration
 
